9ed5c9235d91efefc48001a02bcc93efe0e503f9
Eva Jørgensen <evax1183@elev.techcollege.dk>
2021-11-05 14:42:19 +0100

    upload

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// Slides
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+//Modal
+let modal = document.getElementById("modal");
+let btn = document.getElementById("open");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
